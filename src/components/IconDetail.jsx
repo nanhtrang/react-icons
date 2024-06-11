31562741715113ca9b37d9363eaaa33fe9e31f36
toan.tranduc <toan.tranduc@vti.com.vn>
2024-06-11 15:40:14 +0700

    update

---
+++ src/components/IconDetail.jsx
@@ -0,0 +1,18 @@
+
+
+function IconDetail({name, libName}) {
+  return (
+    <div className="icon-box">
+      <div className="icon">
+        {name}
+      </div>
+      <div className="icon-name">
+        {name}
+      </div>
+    </div>
+  )
+}
+
+
+
+export default IconDetail
--- src/pages/Icon.jsx
+++ src/pages/Icon.jsx
@@ -5,46 +5,53 @@ import { useEffect, useState } from 'react';
 import data from '../assets/dump/data.json'
 import { Row } from 'react-bootstrap';
 import { CopyBlock, googlecode } from 'react-code-blocks';
+import IconDetail from '../components/IconDetail';
 
 
 function Icon() {
   const location = useLocation();
   const [jsonData, setJsonData] = useState({})
+  const [icons, setIcons] = useState([])
+  const [lib, setLib] = useState([])
 
-  function renderIcon() {
-    
-  }
+
 
   useEffect(() => {
     const pathname = location.pathname
     const json = data.find((el) => ('/' + el.path) === pathname)
     setJsonData(json ? json : null)
+    setIcons(json.icons)
+    setLib(json.importSyntax.replace(/.*from "/).replace("\"", ""))
   }, [location])
   return (
     <Layout>
       <Row>
-        <h1 style={{fontWeight: 'normal'}}>{jsonData.name}</h1>
+        <h1 style={{ fontWeight: 'normal' }}>{jsonData.name}</h1>
       </Row>
       <Row>
         <div>License</div>
-        <Link style={{marginLeft: '40px'}} to={jsonData.licenseHref} >{jsonData.license}</Link>
+        <Link style={{ marginLeft: '40px' }} to={jsonData.licenseHref} >{jsonData.license}</Link>
       </Row>
       <Row>
         <div>Project</div>
-        <Link style={{marginLeft: '40px'}} to={jsonData.projectName} >{jsonData.projectName}</Link>
+        <Link style={{ marginLeft: '40px' }} to={jsonData.projectName} >{jsonData.projectName}</Link>
       </Row>
 
       <Row className='mt-2'>
-        <h4 style={{fontWeight: 'normal'}}>Import</h4>
+        <h4 style={{ fontWeight: 'normal' }}>Import</h4>
         <div>
-        <CopyBlock  theme={googlecode} language={'js'} text={jsonData.importSyntax} codeBlock showLineNumbers={false} />
+          <CopyBlock theme={googlecode} language={'js'} text={jsonData.importSyntax} codeBlock showLineNumbers={false} />
         </div>
       </Row>
 
       <Row className='mt-2'>
-        <h4 style={{fontWeight: 'normal'}}>Icons</h4>
-        <div>
-        <CopyBlock  theme={googlecode} language={'js'} text={jsonData.importSyntax} codeBlock showLineNumbers={false} />
+        <h4 style={{ fontWeight: 'normal' }}>Icons</h4>
+        <div className='icon-container'>
+          {icons.map((el, index) => {
+            return (
+              <IconDetail key={index} name={el.name} libName={lib}/>
+            )
+          })}
         </div>
       </Row>
     </Layout>
